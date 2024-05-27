c219a7f7c20b7a4ab4823d09b63314077703453c
Suryanarayanan Nampoothiri <am.en.u4ece22141@am.students.amrita.edu>
2024-05-27 20:16:36 +0530

    Update serial comms.py

--- serial comms.py
+++ serial comms.py
@@ -1,12 +1,15 @@
 import serial
 import time
 ser= serial.Serial(port='COM3',baudrate=9600,timeout=1)
-def writeLmao(x):
+def writeUSB(x):
     text=bytearray(x,'utf-8')
     ser.write(text)
     time.sleep(1)
 
 
 while True:
-    num = input('Enter a number: ')
-    value = writeLmao(num)
+    num = input('Enter value: ')
+    if ((num>1180 and num<2000) or (num>2180 and num<3000) or (num >3180 and num < 4000) or (num >4180 and num < 5000) or (num >5180 and num <6000)):
+        print("Invalid Entry!")
+    else:
+        value = writeUSb(num)
